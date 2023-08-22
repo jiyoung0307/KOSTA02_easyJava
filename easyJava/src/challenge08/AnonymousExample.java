eca85a05f272923bf78f1af16aa91a4cfc9371d4
zy_zero <jiyoungjang0307@gmail.com>
2023-08-22 17:05:23 +0900

    Day9 chap09 확인문제 최종 commit

--- easyJava/src/challenge08/AnonymousExample.java
+++ easyJava/src/challenge08/AnonymousExample.java
@@ -1,19 +1,19 @@
 package easyJava.src.challenge08;
 
 // 이것이 자바다
+// 익명 클래스 연습
 // 9장 6번
 // p423
 public class AnonymousExample {
   public static void main(String[] args) {
+//    부모 안에 있는 익명 객체
     Anonymous anony = new Anonymous();
 //    익명의 객체 필드 사용
-    anony.field.run();
-
+    anony.field.run();    // 자전거가 달립니다.
 //    익명 객체 로컬변수 사용
-    anony.method1();
-
+    anony.method1();      // 승용차가 달립니다.
 //    익명 객체 매개값 사용
-    anony.method2(new Vehicle() {
+    anony.method2(new Vehicle() {   // 트럭 객체 생성
       @Override
       public void run() {
         System.out.println("트럭이 달립니다.");
